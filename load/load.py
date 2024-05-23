1ab34dab3f4295c0776829d57a022ac582bb6a1a
Si Chan Park <danielp315@gmail.com>
2024-05-22 21:05:50 -0400

    Fixed tempfile code

--- load/load.py
+++ load/load.py
@@ -12,35 +12,30 @@ def unzip_json_file(srt_gz_dir, dest_json_dir, zip_file):
         with open(os.path.join(dest_json_dir + "/" +zip_file[:-3]), "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-# Provide the location of my directory to create a temp directory.
-current_directory = "C:\\Users\\danie\\Documents\\Build Fellows Workspace\\github-stars-pipeline\\data"
 
 # Create a temp directory to hold unzipped data.  
-temp_dir =tempfile.mkdtemp(dir = current_directory, prefix="unzipped_data_")
+with tempfile.TemporaryDirectory(prefix="unzipped_data_") as temp_dir:
 
-# Loop through each zipped file and unzip.
-for filename in os.listdir(zippedDirectory):
+    # Loop through each zipped file and unzip.
+    for filename in os.listdir(zippedDirectory):
 
-    # .DS_Store is macOS hidden system files that needs to be skipped when unzipping. 
-    if filename == '.DS_Store':
-        continue
-    unzip_json_file(zippedDirectory, temp_dir, filename)
+        # .DS_Store is macOS hidden system files that needs to be skipped when unzipping. 
+        if filename == '.DS_Store':
+            continue
+        unzip_json_file(zippedDirectory, temp_dir, filename)
+        
+    for filename in os.listdir(temp_dir):
+        print(filename)
 
+    # Create a persistent DuckDB connection to my repo.
+    connection = duckdb.connect("github_stars.db")
 
-# Create a persistent DuckDB connection to my repo.
-connection = duckdb.connect("github_stars.db")
+    # Create a 'source' schema in my 
+    connection.execute("CREATE SCHEMA source")
 
-# Create a 'source' schema in my 
-connection.execute("CREATE SCHEMA source")
+    # Use SQL to create or replace the table source.src_gharchive with all the jsons. 
+    data = os.path.join(temp_dir + "*.json")
+    connection.sql(f"create or replace table source.gharchive as select * from read_json_auto('{data}')")
 
-
-# Use SQL to create or replace the table source.src_gharchive with all the jsons. 
-data = os.path.join(temp_dir + "*.json")
-connection.sql(f"create or replace table source.gharchive as select * from read_json_auto('{data}')")
-
-connection.close()
-
-
-# Deleting the temp directory.
-shutil.rmtree(temp_dir)
+    connection.close()
 
