2126a3f75222dbb283749e7a9c2be54fa3ae3eab
Si Chan Park <danielp315@gmail.com>
2024-05-23 21:37:28 -0400

    Edited the print message

--- load/load.py
+++ load/load.py
@@ -10,6 +10,7 @@ def unzip_json_file(srt_gz_dir, dest_json_dir, zip_file):
     """Function that unzips all gzip files in the srt_gz_dir and stores the unzipped files to dest_json_dir."""
     with gzip.open(srt_gz_dir + zip_file, "rb") as f_in:
         with open(os.path.join(dest_json_dir + "/" +zip_file[:-3]), "wb") as f_out:
+            print(f"Unzipping file {zip_file[:-3]}...")
             shutil.copyfileobj(f_in, f_out)
 
 
@@ -20,9 +21,7 @@ with tempfile.TemporaryDirectory(prefix="unzipped_data_") as temp_dir:
     for filename in os.listdir(zipped_directory):
         unzip_json_file(zipped_directory, temp_dir, filename)
         
-    for filename in os.listdir(temp_dir):
-        print(filename)
-
+        
     # Create a persistent DuckDB connection to my repo.
     connection = duckdb.connect("github_stars.db")
 
