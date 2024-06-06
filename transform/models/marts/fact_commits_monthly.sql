d1a2277d2d8359a3d6c424ec59f8c17cafec3fe3
Si Chan Park <danielp315@gmail.com>
2024-06-05 22:38:24 -0400

    Made changes to calculations

--- transform/models/marts/fact_commits_monthly.sql
+++ transform/models/marts/fact_commits_monthly.sql
@@ -2,8 +2,8 @@ SELECT
   d.date_month, 
   c.repo_id, 
   COUNT(*) AS count_commits, 
-  lag(count_commits, 12) OVER (PARTITION BY c.repo_id ORDER BY d.date_month) AS last_year_count_commits, 
-  count_commits / last_year_count_commits AS yoy_growth, 
+  nullif((lag(count_commits, 12) OVER (PARTITION BY c.repo_id ORDER BY d.date_month)), 0) AS last_year_count_commits, 
+  count_commits / last_year_count_commits -1 AS yoy_growth, 
 FROM {{ ref('dim_date') }} AS d 
   LEFT JOIN {{ ref('fact_commits') }} AS c
   ON d.date_month = DATE_TRUNC('month', c.event_date)
