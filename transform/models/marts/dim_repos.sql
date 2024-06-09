663868f70222277712614d436e52f0792b0a96de
Si Chan Park <danielp315@gmail.com>
2024-06-09 09:06:49 -0400

    Created a type 2 SCD

--- transform/models/marts/dim_repos.sql
+++ transform/models/marts/dim_repos.sql
@@ -1,3 +1,7 @@
-SELECT DISTINCT repo_id,
-    repo_name
-FROM {{ ref("stg_gharchive") }}
+SELECT DISTINCT 
+  repo_id, 
+  repo_name, 
+  min(event_date) AS start_date, 
+  lead(start_date) OVER (PARTITION BY repo_id ORDER BY start_date ASC) AS date_end 
+FROM github_stars.main.stg_gharchive 
+GROUP BY 1, 2
