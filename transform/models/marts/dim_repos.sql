62a5028d5304bdec797e73b4f21d0a1da00a40aa
Si Chan Park <danielp315@gmail.com>
2024-06-14 18:49:36 -0400

    Changed Style

--- transform/models/marts/dim_repos.sql
+++ transform/models/marts/dim_repos.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT 
   repo_id, 
   repo_name, 
-  min(event_date) AS start_date, 
-  lead(start_date) OVER (PARTITION BY repo_id ORDER BY start_date ASC) AS date_end 
+  MIN(event_date) AS start_date, 
+  LEAD(start_date) OVER (PARTITION BY repo_id ORDER BY start_date ASC) AS date_end 
 FROM github_stars.main.stg_gharchive 
 GROUP BY 1, 2
