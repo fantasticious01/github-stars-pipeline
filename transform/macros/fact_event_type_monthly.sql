cc0bb8ae151027c25571f8d1497ed6f75ddfa010
Si Chan Park <danielp315@gmail.com>
2024-06-13 22:03:16 -0400

    Finished bonus step and fixed date bug

--- transform/macros/fact_event_type_monthly.sql
+++ transform/macros/fact_event_type_monthly.sql
@@ -1,14 +1,34 @@
 {% macro fact_event_type_monthly(fact_table) -%} 
-SELECT
-  d.date_month, 
-  f.repo_id, 
-  COUNT(*) AS year_count, 
-  nullif((lag(year_count, 12) OVER (PARTITION BY f.repo_id ORDER BY d.date_month)), 0) AS last_year_count, 
-  year_count / last_year_count -1 AS yoy_growth, 
-FROM {{ ref('dim_date') }} AS d 
-  LEFT JOIN {{ ref(fact_table) }} AS f
-  ON d.date_month = DATE_TRUNC('month', f.event_date)
-GROUP BY 1, 2
+with repo_date_range as (
+  select
+    repo_id,
+    min(event_date) as min_date,
+    max(event_date) as max_date
+  from {{ ref(fact_table)}}
+  group by 1
+),
 
+repo_month_spine as (
+  select
+    rd.repo_id,
+    d.date_month
+  from {{ ref('dim_date') }} as d
+  join repo_date_range as rd
+    on d.date_month
+    between date_trunc('month', rd.min_date)
+    and date_trunc('month', rd.max_date)
+)
+
+select
+  rm.date_month,
+  rm.repo_id,
+  sum(case when s.event_date is null then 0 else 1 end) as year_count,
+  lag(year_count, 12) over (partition by rm.repo_id order by rm.date_month) as last_year_count,
+  (year_count / last_year_count) - 1 as yoy_growth
+from repo_month_spine as rm
+  left join {{ ref(fact_table) }} as s
+  on rm.date_month = date_trunc('month', s.event_date)
+  and rm.repo_id = s.repo_id
+group by 1, 2
 
 {%- endmacro %}
